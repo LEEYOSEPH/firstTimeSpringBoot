b03ea481c860ed6cbb93354925bc1585bbb26600
LEEYOSEPH <laksmi0628@gmail.com>
2022-03-14 21:25:01 +0900

    feat : 스프링 시큐리티를 사용해서 구글 로그인 연동

--- src/main/java/com/first/springboot/web/IndexController.java
+++ src/main/java/com/first/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.first.springboot.web;
 
 import com.first.springboot.dto.PostsResponseDto;
+import com.first.springboot.dto.SessionUser;
 import com.first.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,16 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts",postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
